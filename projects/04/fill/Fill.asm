085d765ccddcfce77f55d8108d783733bf3cc389
mi-nakano <mizuki590@gmail.com>
2018-10-08 13:43:31 +0900

    implement Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+// ----- decide black or white
+@val
+M=0
+@KBD
+D=M
+@DECIDEEND
+D;JEQ
+@val
+M=-1
+(DECIDEEND)
+// decide black or white -----
+
+// current = 16384 screen (0,0)
+@SCREEN
+D=A
+@current
+M=D
+
+// count = 8192 screen size
+@8192
+D=A
+@count
+M=D
+
+// ----- fill screen
+(FILL)
+// load val
+@val
+D=M
+
+// Addr[current] = val
+@current
+A=M
+M=D
+
+// current += 1
+@current
+D=M+1
+@current
+M=D
+
+// check FILL loop
+@count
+M=M-1
+D=M
+@FILL
+D;JGT
+// fill screen -----
+
+// goto LOOP infinitely
+@LOOP
+0;JMP

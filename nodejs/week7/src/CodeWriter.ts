9fd6048482af28b01066d0ddff66eb77f4a92399
mi-nakano <mizuki590@gmail.com>
2018-11-10 10:57:17 +0900

    use temp segment instead of local segment

--- nodejs/week7/src/CodeWriter.ts
+++ nodejs/week7/src/CodeWriter.ts
@@ -21,32 +21,125 @@ export default class CodeWriter {
 
     public writeArithmetic(command: string): void {
         //console.log(`writeArithmetic(${command})`);
+
         if (command === 'add') {
             this.writeAdd();
+        } else if (command === 'sub') {
+            this.writeSub();
+        } else if (command === 'and') {
+            this.writeAnd();
+        } else if (command === 'or') {
+            this.writeOr();
+        } else if (command === 'not') {
+            this.writeNot();
+        } else if (command === 'neg') {
+            this.writeNeg();
+        } else if (command === 'eq') {
+            this.writeEq();
+        } else if (command === 'lt') {
+            this.writeLt();
+        } else if (command === 'gt') {
+            this.writeGt();
         }
-    }
 
-    private writeAdd(): void {
-        this.writePop('local', 0);
-        this.writePop('local', 1);
-
+        // temp 0 <- D
         let output = '';
-        // D = local0 + local1
-        output += '@LCL' + '\n';
-        output += 'A=M+1' + '\n';
-        output += 'D=M' + '\n';
-        output += '@LCL' + '\n';
-        output += 'A=M' + '\n';
-        output += 'D=D+M' + '\n';
-
-        // local 0 <- Result
-        output += '@LCL' + '\n';
-        output += 'A=M' + '\n';
+        output += '@5' + '\n';
         output += 'M=D' + '\n';
         this.write(output);
 
-        this.writePush('local', 0);
+        this.writePush('temp', 0);
     }
+
+    private writeAdd(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=M' + '\n';
+        output += '@6' + '\n';  // temp 1
+        output += 'D=D+M' + '\n';
+        this.write(output);
+    }
+
+    private writeSub(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=M' + '\n';
+        output += '@6' + '\n';  // temp 1
+        output += 'D=D-M' + '\n';
+        this.write(output);
+    }
+
+    private writeAnd(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=M' + '\n';
+        output += '@6' + '\n';  // temp 1
+        output += 'D=D&M' + '\n';
+        this.write(output);
+    }
+
+    private writeOr(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=M' + '\n';
+        output += '@6' + '\n';  // temp 1
+        output += 'D=D|M' + '\n';
+        this.write(output);
+    }
+
+    private writeNot(): void {
+        this.writePop('temp', 0);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=!M' + '\n';
+        this.write(output);
+    }
+
+    private writeNeg(): void {
+        this.writePop('temp', 0);
+
+        let output = '';
+        output += '@5' + '\n';  // temp 0
+        output += 'D=-M' + '\n';
+        this.write(output);
+    }
+
+    private writeEq(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        this.write(output);
+    }
+
+    private writeGt(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        this.write(output);
+    }
+    private writeLt(): void {
+        this.writePop('temp', 0);
+        this.writePop('temp', 1);
+
+        let output = '';
+        this.write(output);
+    }
+
 
     public writePushPop(type: CommandType, segment: string, index: number): void {
         if (type === CommandType.C_PUSH) {
@@ -64,6 +157,10 @@ export default class CodeWriter {
         if (segment === 'constant') {
             output += `@${index}` + '\n';
             output += 'D=A' + '\n';
+        } else if (segment === 'pointer') {
+            output += `@${3 + index}` + '\n'; output += 'D=M' + '\n';
+        } else if (segment === 'temp') {
+            output += `@${5 + index}` + '\n'; output += 'D=M' + '\n';
         } else {
             output += `@${segmentMap[segment]}` + '\n';
             output += 'D=M' + '\n';
@@ -87,12 +184,21 @@ export default class CodeWriter {
     private writePop(segment: string, index: number) {
         //console.log(`writePop(${segment}, ${index})`);
         let output = '';
-        // R5 = segment Addr
-        output += `@${segmentMap[segment]}` + '\n';
-        output += 'D=M' + '\n';
-        output += `@${index}` + '\n';
-        output += 'D=D+A' + '\n';
-        output += '@R5' + '\n';
+
+        // R13 = segment Addr
+        if (segment === 'pointer') {
+            output += `@${3 + index}` + '\n';
+            output += 'D=A' + '\n';
+        } else if (segment === 'temp') {
+            output += `@${5 + index}` + '\n';
+            output += 'D=A' + '\n';
+        } else {
+            output += `@${segmentMap[segment]}` + '\n';
+            output += 'D=M' + '\n';
+            output += `@${index}` + '\n';
+            output += 'D=D+A' + '\n';
+        }
+        output += '@R13' + '\n';
         output += 'M=D' + '\n';
 
         // D = SP--
@@ -101,7 +207,7 @@ export default class CodeWriter {
         output += 'A=M' + '\n';
         output += 'D=M' + '\n';
 
-        output += '@R5' + '\n';
+        output += '@R13' + '\n';
         output += 'A=M' + '\n';
         output += 'M=D' + '\n';
 

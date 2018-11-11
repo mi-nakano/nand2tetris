f9faa53d563f851d633b59100bdf5d6b5f40ac81
mi-nakano <mizuki590@gmail.com>
2018-11-11 21:52:39 +0900

    implement multi parser

--- nodejs/week8/src/VMTranslator.ts
+++ nodejs/week8/src/VMTranslator.ts
@@ -1,47 +1,70 @@
 import Parser from './Parser';
 import CodeWriter from './CodeWriter';
 import { CommandType } from './Enum';
+const fs = require('fs');
 
 if (process.argv.length !== 3) {
     throw new Error();
 }
 
-const fileName = process.argv[2];   // fileName.vm
-const parser = new Parser(`${fileName}`);
-const codeWriter = new CodeWriter();
-codeWriter.setFileName(`${fileName.replace('.vm', '')}.asm`);
+const path = process.argv[2];   // fileName.vm or directory
+const vmFiles: string[] = [];
+let asmFileName;
 
-while (parser.hasMoreCommands()) {
-    parser.advance();
-    switch (parser.commandType()) {
-        case CommandType.C_ARITHMETIC:
-            codeWriter.writeArithmetic(parser.arg1());
-            break;
-        case CommandType.C_PUSH:
-            codeWriter.writePushPop(CommandType.C_PUSH, parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_POP:
-            codeWriter.writePushPop(CommandType.C_POP, parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_LABEL:
-            codeWriter.writeLabel(parser.arg1());
-            break;
-        case CommandType.C_GOTO:
-            codeWriter.writeGoto(parser.arg1());
-            break;
-        case CommandType.C_IF:
-            codeWriter.writeIf(parser.arg1());
-            break;
-        case CommandType.C_CALL:
-            codeWriter.writeCall(parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_RETURN:
-            codeWriter.writeReturn();
-            break;
-        case CommandType.C_FUNCTION:
-            codeWriter.writeFunction(parser.arg1(), parser.arg2());
-            break;
-        default:
-            throw new Error('unknown CommandType');
+if (path.endsWith('.vm')) { // only 1 vm file
+    vmFiles.push(path)
+    asmFileName = path.replace('.vm', '') + '.asm';
+} else {    // all .vm files in directory
+    const dirName = path.replace(/.*\//, '');
+    asmFileName = `${path}/${dirName}.asm`;
+    for (const fileName of fs.readdirSync(path)) {
+        if (fileName.endsWith('.vm')) {
+            vmFiles.push(`${path}/${fileName}`);
+        }
+    }
+}
+
+const codeWriter = new CodeWriter();
+codeWriter.setFileName(asmFileName);
+codeWriter.writeInit();
+for (let vmFile of vmFiles) {
+    const parser = new Parser(`${vmFile}`);
+    translateVM(parser, codeWriter);
+}
+
+function translateVM(parser: Parser, codeWriter: CodeWriter) {
+    while (parser.hasMoreCommands()) {
+        parser.advance();
+        switch (parser.commandType()) {
+            case CommandType.C_ARITHMETIC:
+                codeWriter.writeArithmetic(parser.arg1());
+                break;
+            case CommandType.C_PUSH:
+                codeWriter.writePushPop(CommandType.C_PUSH, parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_POP:
+                codeWriter.writePushPop(CommandType.C_POP, parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_LABEL:
+                codeWriter.writeLabel(parser.arg1());
+                break;
+            case CommandType.C_GOTO:
+                codeWriter.writeGoto(parser.arg1());
+                break;
+            case CommandType.C_IF:
+                codeWriter.writeIf(parser.arg1());
+                break;
+            case CommandType.C_CALL:
+                codeWriter.writeCall(parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_RETURN:
+                codeWriter.writeReturn();
+                break;
+            case CommandType.C_FUNCTION:
+                codeWriter.writeFunction(parser.arg1(), parser.arg2());
+                break;
+            default:
+                throw new Error('unknown CommandType');
+        }
     }
 }

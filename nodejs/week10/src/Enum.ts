1dad5a101cab39b01740552e3fb1d433017ca705
mi-nakano <mizuki590@gmail.com>
2018-11-25 15:13:49 +0900

    implement model

---
+++ nodejs/week10/src/Enum.ts
@@ -0,0 +1,34 @@
+enum TokenType {
+    KEYWORD,
+    SYMBOL,
+    IDENTIFIER,
+    INT_CONST,
+    STRING_CONST,
+}
+
+enum KeyWord {
+    CLASS,
+    METHOD,
+    FUNCTION,
+    CONSTRUCTOR,
+    INT,
+    BOOLEAN,
+    CHAR,
+    VOID,
+    VAR,
+    STATIC,
+    FIELD,
+    LET,
+    DO,
+    IF,
+    ELSE,
+    WHILE,
+    RETURN,
+    TRUE,
+    FALSE,
+    NULL,
+    THIS,
+}
+
+
+export { TokenType, KeyWord };
